810973d002e358e24bbbbed4a8da15a325b82313
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-08-12 17:05:36 +0200

    aggiunto algoritmo di prim per la generazione dei labirinti

--- maze_generator/algorithms.py
+++ maze_generator/algorithms.py
@@ -31,3 +31,36 @@ def generate_maze_dfs(width, height):
             stack.pop()
     
     return maze
+
+def generate_maze_prim(width, height):
+    # Inizializza il labirinto
+    maze = [[0 for _ in range(width)] for _ in range(height)]
+    walls = []
+    
+    # Inizializza il punto di partenza
+    start_x, start_y = 0, 0
+    maze[start_y][start_x] = 1
+    walls.extend([(start_x, start_y, x, y) for x, y in get_neighbors(start_x, start_y, width, height)])
+    
+    while walls:
+        x1, y1, x2, y2 = random.choice(walls)
+        walls.remove((x1, y1, x2, y2))
+        
+        if maze[y2][x2] == 0:
+            maze[y2][x2] = 1
+            maze[(y1 + y2) // 2][(x1 + x2) // 2] = 1
+            walls.extend([(x2, y2, nx, ny) for nx, ny in get_neighbors(x2, y2, width, height) if maze[ny][nx] == 0])
+    
+    return maze
+
+def get_neighbors(x, y, width, height):
+    neighbors = []
+    if x > 1:
+        neighbors.append((x - 2, y))
+    if x < width - 2:
+        neighbors.append((x + 2, y))
+    if y > 1:
+        neighbors.append((x, y - 2))
+    if y < height - 2:
+        neighbors.append((x, y + 2))
+    return neighbors
